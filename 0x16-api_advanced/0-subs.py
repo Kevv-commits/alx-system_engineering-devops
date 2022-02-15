9df2a9ad3bd7c5a395a24b211b77605cb32ad9d2
Kevin Nganga <knganga28@gmail.com>
2022-02-15 19:32:40 +0000

    added description

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the num of subs for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers={'User-Agent': 'k_omett49'},
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'},
                             allow_redirects=False)
     if response.status_code == 200:
         users = response.json().get('data')

4e8d860ee43bdd013fe7e91b9d9a6e5f7799d63d
Kevin Ng'ang'a <knganga28@gmail.com>
2022-02-16 12:35:40 -0800

    made corrections

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# Fixes a faulty wordpress site
+# find out why wp returns a 500 error & find the issue.
+
 exec { 'fix-wordpress':
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php; service apache2 restart"',

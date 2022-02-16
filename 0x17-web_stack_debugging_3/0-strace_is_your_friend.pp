ee146173b2403d83ee35a3ea8ffdaf68eb412558
Kevin Ng'ang'a <knganga28@gmail.com>
2022-02-16 12:39:37 -0800

    fixed again

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
-# find out why wp returns a 500 error & find the issue.
-
+# fix 500 error issue.
 exec { 'fix-wordpress':
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php; service apache2 restart"',

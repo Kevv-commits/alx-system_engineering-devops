2f2abd00e682f940f0677b0fa41537b8e9d38f69
Kevv-commits <knganga28@gmail.com>
2022-02-25 00:21:07 +0000

    fixed last time

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,6 +3,7 @@ exec { 'change limit_1':
   command  => 'sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 88888/g'  /etc/security/limits.conf',
   provider => 'shell'
 }
+
 exec { 'change limit_2':
   command  => 'sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 88888/g'  /etc/security/limits.conf',
   provider => 'shell'

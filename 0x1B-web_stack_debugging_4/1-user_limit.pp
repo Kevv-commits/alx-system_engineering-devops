f51922d5a0e9671d42539c18f7aaf638c18653c0
Kevv-commits <knganga28@gmail.com>
2022-02-25 00:18:45 +0000

    task done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,9 @@
 # User limit
 exec { 'change limit_1':
-  command  => 'sed -i "s/holberton file 5/holberton hard nofile 55000/g" /etc/security/limits.conf',
+  command  => 'sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 88888/g'  /etc/security/limits.conf',
   provider => 'shell'
 }
-
 exec { 'change limit_2':
-  command  => 'sed -i "s/holberton file 4/holberton soft nofile 55000/g" /etc/security/limits.conf',
+  command  => 'sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 88888/g'  /etc/security/limits.conf',
   provider => 'shell'
 }
